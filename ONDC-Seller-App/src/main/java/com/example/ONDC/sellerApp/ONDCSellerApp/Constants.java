93e8d01f4dafb0edd229e410ce4d0bd20375c6cc
Thelukuntla Phaneendra <thelukuntla.phaneendra@paytm.com>
2023-07-06 01:04:14 +0530

    ConstantsClassAdded

---
+++ ONDC-Seller-App/src/main/java/com/example/ONDC/sellerApp/ONDCSellerApp/Constants.java
@@ -0,0 +1,27 @@
+package com.example.ONDC.sellerApp.ONDCSellerApp;
+
+public class Constants {
+  public static final String BASE_URL = "/api/v1/ai";
+  public static final String GENERATE_IMAGE = "/generate/image";
+  public static final String REMOVE_BACKGROUND = "/remove/background";
+  public static final String OPEN_AI_IMAGE_REDIRECTION_URL = "operation-location";
+  public static final String API_KEY = "api-key";
+  public static final String X_API_KEY = "x-api-key";
+  public static final String API_VERSION = "api-version";
+  public static final String ENHANCE_TITLE = "/enhance/title";
+  public static final String CHAT_COMPLETION_API_VERSION = "2023-03-15-preview";
+  public static final String CONTENT_TYPE = "Content-Type";
+  public static final String APPLICATION_JSON = "application/json";
+  public static final String GENERATE_DESCRIPTION = "/generate/description";
+  public static final String ENHANCE_DESCRIPTION = "/enhance/description";
+  public static final String GENERATE_ADDITIONAL_DESCRIPTION = "/generate/additionalDescription";
+
+  public static class ProductControllerConstants {
+    public static final String PRODUCT_BASE_URL = "/api/v1/product";
+  }
+
+  public static class PromtGenerationConstants {
+    public static final String USER_ROLE = "user";
+    public static final String SYSTEM_ROLE = "system";
+  }
+}
