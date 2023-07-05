23610cb7343356c6031999a2d41d9e99517ce669
sajal <sajal.passi@paytm.com>
2023-07-05 12:07:36 +0530

    Added maven project

---
+++ ONDC-Seller-App/src/main/java/com/example/ONDC/sellerApp/ONDCSellerApp/OndcSellerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ONDC.sellerApp.ONDCSellerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OndcSellerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OndcSellerAppApplication.class, args);
+	}
+
+}
