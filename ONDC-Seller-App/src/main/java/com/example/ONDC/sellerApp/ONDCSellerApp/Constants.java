9b724bcc27578ced2e57c3df1d424d8c114786e7
Abhiraj Verma <abhiraj.verma@paytm.com>
2023-07-05 18:37:51 +0530

    rename constant file

--- ONDC-Seller-App/src/main/java/com/example/ONDC/sellerApp/ONDCSellerApp/Constants.java
+++ ONDC-Seller-App/src/main/java/com/example/ONDC/sellerApp/ONDCSellerApp/Constants.java
@@ -6,4 +6,9 @@ public class constants {
   public static final String OPEN_AI_IMAGE_REDIRECTION_URL = "operation-location";
   public static final String API_KEY = "api-key";
   public static final String API_VERSION = "api-version";
+
+  public static class ProductControllerConstants {
+    public static final String PRODUCT_BASE_URL = "/api/v1/product";
+    
+  }
 }
